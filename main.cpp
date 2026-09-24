3a275b6361b579523446ce69fedb2e90f2a338b5
Erikas <erikasdenisenko@gmail.com>
2026-09-24 16:03:19 +0300

    Patobulintas studentu duomenu saugojimas

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
+
 using namespace std;
 struct Studentas {
     string vardas;
@@ -11,55 +13,72 @@ struct Studentas {
 };
 
 int main () {
-    Studentas studentas;
-    cout << "Iveskite studento varda";
-    cin >> studentas.vardas;
-    cout << "Iveskite studento pavarde";
-    cin >> studentas.pavarde;
-    cout << "Iveskite studento namu darbu pazymius (ivesti 0 - pabaigti):" << endl;
-    int pazymys;
-
+    vector <Studentas> studentai;
     while (true) {
-        cin >> pazymys; 
-        if (pazymys == 0)  {
-            break;  
-        }
+        Studentas studentas;
 
-        if (pazymys >=1 && pazymys <= 10) {
-            studentas.nd.push_back(pazymys);
-        } else {
+        cout << "Iveskite studento varda: ";
+        cin >> studentas.vardas;
+
+        cout <<"Iveskite studento pavarde: ";
+        cin >> studentas.pavarde;
+
+        cout << "Iveskite studento namu darbu pazymius (norint pabaigti - iveskite 0):" << endl;
+
+        int pazymys;
+
+        while (true) {
+            cin >> pazymys;
+
+            if (pazymys == 0 && !studentas.nd.empty()) {
+                break;
+
+            }
+            if (pazymys >= 1 && pazymys <= 10) {
+                studentas.nd.push_back(pazymys);
+            } else {
+                cout << "Pazymys turi buti nuo 1 iki 10." << endl;
+            }
+        }
+        cout <<" Iveskite studento egzamino pazymi: ";
+        
+        while (true) {
+            cin >> studentas.egzaminas;
+
+            if (studentas.egzaminas >= 1 && studentas.egzaminas <= 10 ) {
+                break;
+            }
             cout << "Pazymys turi buti nuo 1 iki 10." << endl;
-
         }
+        studentai.push_back(studentas);
 
-    }
-    cout << "Iveskite studento egzamino pazymi: ";
+        char pasirinkimas;
+        cout << "Ar norite ivesti dar viena studenta? (t/n): ";
+        cin >> pasirinkimas;
 
-    while (true) {
-        cin  >> studentas.egzaminas;
-
-        if (studentas.egzaminas >=1 && studentas.egzaminas <= 10) {
+        if(pasirinkimas == 'n') {
             break;
-
         }
-        cout << "Pazymys turi buti nuo 1 iki 10." << endl;
-
-
     }
-    double ndVidurkis = 0;
-
-    for (int pazymys : studentas.nd) {
-        ndVidurkis = ndVidurkis + pazymys;
-    }
-    ndVidurkis = ndVidurkis / studentas.nd.size();
-
-    double galutinis = 0.4 * ndVidurkis + 0.6 * studentas.egzaminas;
-
-    cout << fixed << setprecision(2);
-    cout << "Galutinis pazymys : " << galutinis << endl;
     
+    for (Studentas studentas : studentai) {
+        double ndVidurkis = 0;
+    
+        for (int pazymys : studentas.nd) {
+            ndVidurkis += pazymys;
+    }
+    
+        ndVidurkis /= studentas.nd.size();
+
+        cout << studentas.vardas << " " << studentas.pavarde
+           << " ND vidurkis: " << ndVidurkis << endl;
+    }
+
+
 
 }
 
     
 
+
+ 
